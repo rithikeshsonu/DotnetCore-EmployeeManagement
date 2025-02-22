797085db17ae0c6f1b3995ebfbc7ffd7c8fa18e6
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-02-22 14:13:34 +0530

    Configurations
    Order of precedence:
    1. Files (appsettings.json, appsettings.{Environment}.json)
    2. User Secrets
    3. Env variables (Launch Settings.json)
    4. Command-Line args
    
    2 overrides 1, 3 overrides 2, 4 overrides 3. Likewise. If the same value is present in other files. The next value overrides the previous as mentioned in the order above.
    
    CLI Command
    >dotnet run MyKey="Value from CLI"

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 IConfiguration configuration = builder.Configuration;
-string myValue = configuration["MyKey"];
+string myValue = configuration["MyKey"] ?? "DefaultValue";
 var app = builder.Build();
 
 app.MapGet("/", () => $"Hello World! My secret value is: {myValue}");
