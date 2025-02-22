dc7b2b6439890e9524f49e19f973607f885116d4
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-02-22 14:49:07 +0530

    Middleware

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.Extensions.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
+
 IConfiguration configuration = builder.Configuration;
 string myValue = configuration["MyKey"] ?? "DefaultValue";
 var app = builder.Build();
 
-app.MapGet("/", () => $"Hello World! My secret value is: {myValue}");
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation(myValue);
+
+app.MapGet("/", () => $"Hello World");
+
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("Hello from First middleWare");
+    await next();
+});
 
 app.Run();
-
