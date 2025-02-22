78e157687b2341ad9dc3027e58288a76d89aea92
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-02-22 15:44:35 +0530

    Static files

--- Program.cs
+++ Program.cs
@@ -2,19 +2,27 @@ using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
-IConfiguration configuration = builder.Configuration;
-string myValue = configuration["MyKey"] ?? "DefaultValue";
+//IConfiguration configuration = builder.Configuration;
+//string myValue = configuration["MyKey"] ?? "DefaultValue";
 var app = builder.Build();
 
-var logger = app.Services.GetRequiredService<ILogger<Program>>();
-logger.LogInformation(myValue);
+//var logger = app.Services.GetRequiredService<ILogger<Program>>();
+//logger.LogInformation(myValue);
+//app.MapGet("/", () => $"Hello World");
+//To have your custom default files
+DefaultFilesOptions defaultFilesOptions = new();
+defaultFilesOptions.DefaultFileNames.Clear();
+defaultFilesOptions.DefaultFileNames.Add("foo.html");
 
-app.MapGet("/", () => $"Hello World");
+app.UseDefaultFiles(defaultFilesOptions);
+//app.UseDefaultFiles(); //renders index.html or default.html file by default from wwwroot folder
+app.UseStaticFiles(); //To serve images in wwwroot folder
 
-app.Use(async (context, next) =>
-{
-    await context.Response.WriteAsync("Hello from First middleWare");
-    await next();
-});
+
+//app.Use(async (context, next) =>
+//{
+//    await context.Response.WriteAsync("Hello from First middleWare");
+//    await next();
+//});
 
 app.Run();
