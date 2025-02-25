d50babaa3b99e6392372d219c392afa2d6f8719b
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-02-25 23:55:38 +0530

    Adding validation for select item(Dropdown)

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -8,6 +8,6 @@ namespace EmployeeManagement.Models
         public string Name { get; set; } = string.Empty;
         [Required, Display(Name = "Office Email"), RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid email format")]
         public string Email { get; set; } = string.Empty;
-        public Dept Department { get; set; }
+        public Dept? Department { get; set; }
     }
 }
